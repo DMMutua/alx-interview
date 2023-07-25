75e0e21f1d4c458f6946dce2bb7d2ba470e2cbf5
DMMutua <lengowriter99@gmail.com>
2023-07-25 11:40:59 +0300

    pycodestyle compliancy and shebang line

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python3
+
 import sys
 
+
 def is_safe(board, row, col):
-    # Check if a queen can be placed at the given row and column without attacking others
+    """Check if a queen can be placed at the
+    given row and column without attacking others"""
     for i in range(row):
         if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
     return True
 
+
 def nqueens_util(N, row, board):
+    """Utility to Print Directly"""
     if row == N:
         # All queens are placed, print the solution
         print([[i, board[i]] for i in range(N)])
@@ -18,7 +24,9 @@ def nqueens_util(N, row, board):
             board[row] = col
             nqueens_util(N, row + 1, board)
 
+
 def nqueens(N):
+    """Main Solver Function"""
     if not N.isdigit():
         print("N must be a number")
         sys.exit(1)
@@ -31,10 +39,10 @@ def nqueens(N):
     board = [-1] * N
     nqueens_util(N, 0, board)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     nqueens(sys.argv[1])
-
