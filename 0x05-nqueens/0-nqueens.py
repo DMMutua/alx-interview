fad9c4d320d34ff39e69cb0afb3b950d4869b672
DMMutua <lengowriter99@gmail.com>
2023-07-25 11:37:54 +0300

    using backtracking algorithm: removing solutions list and printing directly

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,32 +1,24 @@
-#!/usr/bin/python3
-"""Program to Solve the N Queens Problem"""
-
 import sys
 
-
 def is_safe(board, row, col):
-    """Check if a queen can be placed at the
-    given row and column without attacking others"""
+    # Check if a queen can be placed at the given row and column without attacking others
     for i in range(row):
         if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
     return True
 
-
-def nqueens_util(N, row, board, solutions):
-    """Utility Function"""
+def nqueens_util(N, row, board):
     if row == N:
-        solutions.append([[i, board[i]] for i in range(N)])
+        # All queens are placed, print the solution
+        print([[i, board[i]] for i in range(N)])
         return
 
     for col in range(N):
         if is_safe(board, row, col):
             board[row] = col
-            nqueens_util(N, row + 1, board, solutions)
-
+            nqueens_util(N, row + 1, board)
 
 def nqueens(N):
-    """Main Solver Function"""
     if not N.isdigit():
         print("N must be a number")
         sys.exit(1)
@@ -37,12 +29,7 @@ def nqueens(N):
         sys.exit(1)
 
     board = [-1] * N
-    solutions = []
-    nqueens_util(N, 0, board, solutions)
-
-    for solution in solutions:
-        print(solution)
-
+    nqueens_util(N, 0, board)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -50,3 +37,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     nqueens(sys.argv[1])
+
